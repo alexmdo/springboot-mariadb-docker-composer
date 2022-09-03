d5a159ce5597162db601bd1ba6e7b9b78cccf446
Alexandre Oliveira <alexmdo@gmail.com>
2022-09-03 19:42:11 -0300

    Initial commit

---
+++ src/main/java/com/springboot/springbootmariadbdockercomposer/SpringbootMariadbDockerComposerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootmariadbdockercomposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMariadbDockerComposerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMariadbDockerComposerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/springbootmariadbdockercomposer/SpringbootMariadbDockerComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootmariadbdockercomposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMariadbDockerComposerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
